a5d0d45da2705a6a024f5e1fc3c516ffc3e1528e
Daniel Farage <daniel.farage@yahoo.com.br>
2021-04-15 20:15:51 -0300

    Tela lista de contatos

---
+++ app/src/main/java/com/daniel/farage/financeapp/contactlist/ContactListFragment.kt
@@ -0,0 +1,7 @@
+package com.daniel.farage.financeapp.contactlist
+
+import androidx.fragment.app.Fragment
+import com.daniel.farage.financeapp.R
+
+class ContactListFragment: Fragment(R.layout.fragment_client_list) {
+}
